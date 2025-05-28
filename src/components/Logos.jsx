98174f11bb417b48d8e07954120b741a174386ea
Steve D <youremail@example.com>
2025-05-28 08:58:19 -0700

    Updates

--- src/components/Logos.jsx
+++
@@ -1,9 +0,0 @@
-export default function Logos() {
-  return (
-    <div className="flex justify-center gap-12 items-center py-10 bg-gray-100">
-      <img src="harvard.png" alt="Harvard" className="h-12 md:h-16 object-contain" />
-      <img src="mit.png" alt="MIT" className="h-12 md:h-16 object-contain" />
-      <img src="milken.png" alt="Milken" className="h-12 md:h-16 object-contain" />
-    </div>
-  );
-}
