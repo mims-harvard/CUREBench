daa7d21205c0b91f1537027a8c92fb53cd4a4c48
Shanghua <shgao@live.com>
2025-06-10 21:33:29 -0400

    updat

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -3,7 +3,7 @@ export default function Logos() {
     <div className="flex justify-center gap-12 items-center py-10 bg-gray-100">
       <img src="harvard.png" alt="Harvard" className="h-12 md:h-16 object-contain" />
       <img src="mit.png" alt="MIT" className="h-12 md:h-16 object-contain" />
-      <img src="milken.png" alt="Milken" className="h-12 md:h-16 object-contain" />
+      {/* <img src="milken.png" alt="Milken" className="h-12 md:h-16 object-contain" /> */}
     </div>
   );
 }
